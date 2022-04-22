7f3329817e661d9516581dad44db0ad1df79535c
joeyvijayam <45520122+joeyvijayam@users.noreply.github.com>
2022-04-22 03:18:56 -0400

    Update messages.py

--- common/messages.py
+++ common/messages.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python
+"""
+    Description:
+        This module implements messages and other shared network settings.
+"""
 
 import json
 import time
@@ -8,6 +12,7 @@ KILL_MESSAGE_ID = 2
 FIRE_WEAPON_ID = 3
 UAV_STATUS_ID = 101
 GROUND_RADAR_STATUS_ID = 201
+NETWORK_PORT_NUMBER = 8000
 
 class Message:
     """
@@ -16,39 +21,94 @@ class Message:
     """
     def msg(self) -> str:
         """
-        Description: Return class in JSON format for sending.
+        Description:
+            Return class in JSON format for sending.
         """
         msg_dict = self.__dict__
         msg_dict['timestamp'] = time.time()
         return json.dumps(msg_dict)
 
 class SetIffMessage(Message):
+    """
+        Description:
+           IFF message.
+    """
+
     def __init__(self, is_friend: bool = True):
+        """
+        Description:
+           Create IFF message.
+        """
         self.is_friend = is_friend
         self.message_type: int = SET_IFF_MSG_ID
 
 class KillMessage(Message):
+    """
+    Description:
+        Kill message.
+    """
+
     def __init__(self):
+        """
+        Description:
+           Create kill message.
+        """
         self.message_type: int = KILL_MESSAGE_ID
 
 class FireWeaponMessage(Message):
+    """
+    Description:
+        Fire message.
+    """
+
     def __init__(self):
+        """
+        Description:
+           Create fire message.
+        """
         self.message_type: int = FIRE_WEAPON_ID
 
 class UavStatusMessage(Message):
+    """
+    Description:
+        UAV status message.
+    """
+
     def __init__(self, is_hit: bool = True, is_friend: bool = True):
+        """
+        Description:
+           Create UAV status message.
+        """
         self.message_type: int = UAV_STATUS_ID
         self.is_hit = is_hit
         self.is_friend = is_friend
 
 class RadarData:
-    def __init__(self, range: float = 0, elevation: float = 0, azimuth: float = 0):
-        self.range = range
-        self.elevation = elevation
-        self.azimuth = azimuth
+    """
+    Description:
+        Radar data.
+    """
+
+    def __init__(self, detected_range: float = 0, detected_elevation: float = 0, detected_azimuth: float = 0):
+        """
+        Description:
+            Create a radar data object.
+        """
+        self.detected_range = detected_range
+        self.detected_elevation = detected_elevation
+        self.detected_azimuth = detected_azimuth
 
 class GroundRadarStatus(Message):
+    """
+    Description:
+        Radar status message.
+    """
+
     def __init__(self, radar_data: RadarData, weapon_armed: bool = False):
+        """
+        Description:
+            Create a radar status message.
+        """
         self.message_type: int = GROUND_RADAR_STATUS_ID
         self.radar_data = radar_data
         self.weapon_armed = weapon_armed
