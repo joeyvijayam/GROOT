d20a7367582c7c00cfe1a35193e0d621729ecccd
joeyvijayam <45520122+joeyvijayam@users.noreply.github.com>
2022-05-01 14:12:37 -0400

    Update c2_server.py

--- server/c2_server.py
+++ server/c2_server.py
@@ -9,8 +9,7 @@ import socket
 import json
 import websockets
 
-
-from common.messages import GROUND_RADAR_STATUS_ID, UAV_STATUS_ID, NETWORK_PORT_NUMBER
+from src.py.common.messages import GROUND_RADAR_STATUS_ID, UAV_STATUS_ID, NETWORK_PORT_NUMBER
 
 class C2Server:
     """
